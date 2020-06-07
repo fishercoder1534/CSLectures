1f3df214ae44c7a17cfd1050d4377853a0a763d7
fishercoder1534 <fishercoder@gmail.com>
2020-06-06 17:52:08 -0700

    add insertion sort

--- src/main/java/com/fishercoder/sorting/InsertionSort.java
+++ src/main/java/com/fishercoder/sorting/InsertionSort.java
@@ -1,7 +1,31 @@
 package com.fishercoder.sorting;
 
+import com.fishercoder.common.utils.CommonUtils;
+
 public class InsertionSort {
+
+    /**
+     * To verify its correctness of sorting,
+     * I've used this: https://leetcode.com/problems/largest-perimeter-triangle/
+     */
+    public static int[] insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+        return array;
+    }
+
     public static void main(String... args) {
-        System.out.println("Hello World!");
+        int[] input = new int[]{5, 2, 4, 6, 1, 3};
+        CommonUtils.printArray("Before sorting: ", input);
+        int[] sorted = insertionSort(input);
+        CommonUtils.printArray("After sorting:", sorted);
+
     }
 }
