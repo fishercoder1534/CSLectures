369b754271346bb8c332cf34f9594444cb7a6341
Fisher Coder <fishercoder@gmail.com>
2020-07-19 17:46:24 -0700

    add quick sort

---
+++ src/main/java/com/fishercoder/sorting/QuickSort.java
@@ -0,0 +1,61 @@
+package com.fishercoder.sorting;
+
+import com.fishercoder.common.utils.CommonUtils;
+
+public class QuickSort {
+
+    /**
+     * To verify its correctness of sorting,
+     * I've used this: https://leetcode.com/problems/largest-perimeter-triangle/
+     */
+    public int[] quickSort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+        return array;
+    }
+
+    private void quickSort(int[] array, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(array, begin, end);
+            quickSort(array, begin, partitionIndex - 1);
+            quickSort(array, partitionIndex + 1, end);
+        }
+    }
+
+    private int partition(int[] array, int begin, int end) {
+        int pivot = array[end];
+        int i = begin - 1;
+        for (int j = begin; j < end; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+
+        swap(array, i + 1, end);
+        return i + 1;
+    }
+
+    private void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+
+    public static void main(String... args) {
+        QuickSort quickSort = new QuickSort();
+        int[] input = new int[]{3, 1, 4, 5, 2};
+        CommonUtils.printArray("Before sorting: ", input);
+        int[] sorted = quickSort.quickSort(input);
+        CommonUtils.printArray("After sorting:", sorted);
+
+        input = new int[]{5, 4, 3, 2, 1};
+        CommonUtils.printArray("Before sorting: ", input);
+        sorted = quickSort.quickSort(input);
+        CommonUtils.printArray("After sorting:", sorted);
+
+        input = new int[]{1, 2, 3, 4, 5};
+        CommonUtils.printArray("Before sorting: ", input);
+        sorted = quickSort.quickSort(input);
+        CommonUtils.printArray("After sorting:", sorted);
+    }
+}
